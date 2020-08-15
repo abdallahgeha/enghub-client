d68d8b2cbb3e398437a487c1ca9fb4ce560c9fab
abdallahgeha <geha.abdallah@gmail.com>
2020-08-15 22:02:57 +0200

    next step

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style/index.css';
 import App from './App';
+import "./style/index.css";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />,
+    document.getElementById('root')
 );
